ab428ec1479d9bf6d0f57d341b19d8896f2575ab
Hien <nguyenthuhien5612@gmail.com>
2025-05-16 10:42:26 +0700

    [fix] http-status

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -7,9 +7,9 @@ exports.signup = async (req, res) => {
   try {
     const result = await userService.registerUser(name, password, email);
     if (result.error) {
-      return res.status(400).json({ message: result.error });
+      return res.status(result.status || 400).json({ message: result.error });
     }
-    res.status(201).json({ message: result.message });
+    res.status(result.status || 201).json({ message: result.message });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Lỗi server" });
@@ -22,9 +22,9 @@ exports.login = async (req, res) => {
   try {
     const result = await userService.loginUser(name, password);
     if (result.error) {
-      return res.status(400).json({ message: result.error });
+      return res.status(result.status || 400).json({ message: result.error });
     }
-    res.json({ token: result.token, message: result.message });
+    res.status(result.status || 200).json({ token: result.token, message: result.message });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Lỗi server" });
